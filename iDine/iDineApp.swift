4ffd26daa7ab619065309fe8a7addd0b62e36c39
Ryan Bachrach <ryanbachrach9@gmail.com>
2021-05-26 15:19:31 -0500

    finished step 6

--- iDine/iDineApp.swift
+++ iDine/iDineApp.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
+
 @main
 struct iDineApp: App {
+    @StateObject var order = Order()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(order)
         }
     }
 }
+
